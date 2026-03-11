d19c317e747578b8d1a09f4f1b367ce027becbae
mehar <ms1526@srmist.edu.in>
2026-03-11 09:34:55 +0530

    Refactored UC6 using helper methods for characteristic patterns

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,60 +1,66 @@
 /**
- * OOPSBannerApp prints OOPS banner using inline array initialization.
+ * OOPSBannerApp prints OOPS banner using helper methods.
  *
- * @author YourName
- * @version 5.0
+ * @author Mehar
+ * @version 6.0
  */
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        String[] bannerLines = {
-                String.join(" ",
-                        " ******** ",
-                        " ******** ",
-                        " ******** ",
-                        " ******** "
-                ),
-                String.join(" ",
-                        "**      **",
-                        "**      **",
-                        "**      **",
-                        "**       "
-                ),
-                String.join(" ",
-                        "**      **",
-                        "**      **",
-                        "**      **",
-                        "**       "
-                ),
-                String.join(" ",
-                        "**      **",
-                        "**      **",
-                        "******** ",
-                        " ********"
-                ),
-                String.join(" ",
-                        "**      **",
-                        "**      **",
-                        "**       ",
-                        "       **"
-                ),
-                String.join(" ",
-                        "**      **",
-                        "**      **",
-                        "**       ",
-                        "       **"
-                ),
-                String.join(" ",
-                        " ******** ",
-                        " ******** ",
-                        "**       ",
-                        " ********"
-                )
-        };
+        String[] oPattern = getOPattern();
+        String[] pPattern = getPPattern();
+        String[] sPattern = getSPattern();
+
+        String[] bannerLines = new String[7];
+
+        for (int i = 0; i < 7; i++) {
+            bannerLines[i] = String.join(" ",
+                    oPattern[i],
+                    oPattern[i],
+                    pPattern[i],
+                    sPattern[i]
+            );
+        }
 
         for (String line : bannerLines) {
             System.out.println(line);
         }
+    }
+
+    public static String[] getOPattern() {
+        return new String[]{
+                " ******** ",
+                "**      **",
+                "**      **",
+                "**      **",
+                "**      **",
+                "**      **",
+                " ******** "
+        };
+    }
+
+    public static String[] getPPattern() {
+        return new String[]{
+                " ******** ",
+                "**      **",
+                "**      **",
+                "******** ",
+                "**       ",
+                "**       ",
+                "**       "
+        };
+    }
+
+    public static String[] getSPattern() {
+        return new String[]{
+                " ******** ",
+                "**       ",
+                "**       ",
+                " ********",
+                "       **",
+                "       **",
+                " ********"
+        };
     }
 }
